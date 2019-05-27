0718f271c16e6ab85bb7037a6f1888083a276f22
Ruslan <r_nazar92@mail.ru>
2019-05-27 13:29:54 +0300

    V28 Code CustomerDAO class + Unit test

---
+++ BookStoreWebsite/src/com/bookstore/dao/CustomerDAO.java
@@ -0,0 +1,43 @@
+package com.bookstore.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManagerFactory;
+
+import com.bookstore.entity.Customer;
+
+public class CustomerDAO extends JpaDAO<Customer> implements GenericDAO<Customer> {
+
+	@Override
+	public Customer create(Customer customer) {
+		customer.setRegisterDate(new Date()); // Before calling super.create we need to set register date for the
+												// customer
+		return super.create(customer);
+	}
+
+	@Override
+	public Customer get(Object id) {
+		return super.find(Customer.class, id);
+	}
+
+	public Customer update(Customer customer) {
+		return super.update(customer);
+	}
+
+	@Override
+	public void delete(Object id) {
+		super.delete(Customer.class, id);
+	}
+
+	@Override
+	public List<Customer> listAll() {
+		return super.findWithNamedQuery("Customer.findAll");
+	}
+
+	@Override
+	public long count() {
+		return super.countWithNamedQuery("Customer.countAll");
+	}
+
+}
---
+++ BookStoreWebsite/test/com/bookstore/dao/CustomerDAOTest.java
@@ -0,0 +1,98 @@
+package com.bookstore.dao;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.bookstore.entity.Customer;
+
+public class CustomerDAOTest {
+	private static CustomerDAO customerDAO;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		customerDAO = new CustomerDAO();
+	}
+
+	@Test
+	public void testCreateCustomer() {
+		Customer customer = new Customer();
+
+		customer.setEmail("billy.jane@test.ru");
+		customer.setFullname("Jane Billy");
+		customer.setAddress("Baker Street");
+		customer.setCity("London");
+		customer.setCountry("England");
+		customer.setPhoneNumber("8-900-3000-500");
+		customer.setZipCode("101021");
+		customer.setPassword("sh");
+
+		Customer savedCustomer = customerDAO.create(customer);
+
+		assertTrue(savedCustomer.getCustomerId() > 0);
+	}
+
+	@Test
+	public void testUpdateCustomer() {
+		Customer customer = customerDAO.get(2);
+		customer.setEmail("tom@mail.ru");
+		customer.setFullname("Tom Eager");
+		customer.setAddress("100 North Av.");
+		customer.setCity("New York");
+		customer.setCountry("USA");
+		customer.setPhoneNumber("8-951-910-57-57");
+		customer.setZipCode("607010");
+		customer.setPassword("123");
+		Customer updatedCustomer = customerDAO.update(customer);
+
+		assertTrue(updatedCustomer.getFullname().equals("Tom Eager"));
+	}
+
+	@Test
+	public void testGet() {
+		int customerId = 1;
+
+		Customer customer = customerDAO.get(customerId);
+
+		assertNotNull(customer);
+
+	}
+
+	@Test
+	public void testDeleteObject() {
+		int customerId = 1;
+
+		customerDAO.delete(customerId);
+
+		Customer customer = customerDAO.get(customerId);
+
+		assertNull(customer);
+	}
+
+	@Test
+	public void testListAll() {
+		List<Customer> listCustomers = customerDAO.listAll();
+
+		for (Customer item : listCustomers) {
+			System.out.println(item.getFullname());
+		}
+
+		assertFalse(listCustomers.isEmpty());
+	}
+	
+	@Test
+	public void testCountAll() {
+		long totalCustomers = customerDAO.count();
+		assertEquals(2, totalCustomers);
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		customerDAO.close();
+	}
+
+}
